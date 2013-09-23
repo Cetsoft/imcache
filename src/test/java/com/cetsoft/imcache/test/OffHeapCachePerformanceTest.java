f02c6b3d43f5c015a628db6bf5a572977febd4bd
Yusuf Aytas <yusufaytas@gmail.com>
2013-09-23 15:15:03 +0300

    updated javadocs.

--- src/test/java/com/cetsoft/imcache/test/OffHeapCachePerformanceTest.java
+++ src/test/java/com/cetsoft/imcache/test/OffHeapCachePerformanceTest.java
@@ -1,3 +1,23 @@
+/*
+* Copyright (C) 2013 Cetsoft, http://www.cetsoft.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : Sep 23, 2013
+*/
 package com.cetsoft.imcache.test;
 
 import java.io.Serializable;
@@ -11,8 +31,17 @@ import com.cetsoft.imcache.cache.Cache;
 import com.cetsoft.imcache.cache.builder.CacheBuilder;
 import com.cetsoft.imcache.serialization.Serializer;
 
+/**
+ * The Class OffHeapCachePerformanceTest.
+ */
 public class OffHeapCachePerformanceTest {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws InterruptedException the interrupted exception
+	 */
 	public static void main(String [] args) throws InterruptedException{
 		final int NO_OF_THREADS = 100;
 		final int SIZE = 1000;
@@ -63,7 +92,14 @@ public class OffHeapCachePerformanceTest {
 		System.out.println("Map = "+totalTimeForMap.get());
 	}
 	
+	/**
+	 * The Class SimpleObject.
+	 */
 	private static class SimpleObject implements Serializable{
-		protected int x =3,y=4; String str = "";
+		
+		/** The y. */
+		protected int x =3,y=4; 
+ /** The str. */
+ String str = "";
 	}
 }
