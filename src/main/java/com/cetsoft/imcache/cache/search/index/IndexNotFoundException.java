edf6565627e6dbe5ba0ffb4dc6693521f403edf3
Yusuf Aytas <yusufaytas@gmail.com>
2013-11-01 09:36:32 +0200

    IndexNotFoundException is added.

---
+++ src/main/java/com/cetsoft/imcache/cache/search/index/IndexNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cetsoft.imcache.cache.search.index;
+
+public class IndexNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = -5242844304997610867L;
+
+}
