c276f220c09a8c14eecfaaadf66cd53b1751ffe0
Yusuf Aytas <yusufaytas@gmail.com>
2013-11-18 14:55:53 +0200

    serializer is removed.

--- src/main/java/com/cetsoft/imcache/examples/VersionedOffHeapCacheExample.java
+++ src/main/java/com/cetsoft/imcache/examples/VersionedOffHeapCacheExample.java
@@ -9,22 +9,14 @@ import com.cetsoft.imcache.cache.SimpleItem;
 import com.cetsoft.imcache.cache.VersionedItem;
 import com.cetsoft.imcache.cache.builder.CacheBuilder;
 import com.cetsoft.imcache.cache.offheap.StaleItemException;
-import com.cetsoft.imcache.serialization.Serializer;
 
 public class VersionedOffHeapCacheExample {
 	
 	public static void main(String [] args){
 		OffHeapByteBufferStore bufferStore = new OffHeapByteBufferStore(10000,10); 
 		final Cache<String, VersionedItem<String>> cache =CacheBuilder.versionedOffHeapCache().
-		storage(bufferStore).serializer(new Serializer<String>(){
-			public byte[] serialize(String value) {
-				return com.cetsoft.imcache.examples.Serializer.serialize(value);
-			}
-			public String deserialize(byte[] payload) {
-				return com.cetsoft.imcache.examples.Serializer.deserialize(payload);
-			}
-		}).build();
-		ExecutorService service = Executors.newFixedThreadPool(200);
+		storage(bufferStore).build();
+		ExecutorService service = Executors.newFixedThreadPool(3);
 		for (int i = 0; i < 100000; i++) {
 			service.execute(new Runnable() {
 				public void run() {
