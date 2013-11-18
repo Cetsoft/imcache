2b77488084acc644b0cf1df0f3a84da09fab6a9d
Yusuf Aytas <yusufaytas@gmail.com>
2013-11-18 11:34:16 +0200

    CacheFactory is added to the LocalCacheCoordinator.

--- src/main/java/com/cetsoft/imcache/cache/coordinator/LocalCacheCoordinator.java
+++ src/main/java/com/cetsoft/imcache/cache/coordinator/LocalCacheCoordinator.java
@@ -30,34 +30,48 @@ import com.cetsoft.imcache.cache.CacheType;
  * The Class LocalCacheCoordinator provides local caching for the threads.
  * This coordinator can only be used for non-concurrent caching.
  */
+@SuppressWarnings("rawtypes")
 public class LocalCacheCoordinator implements CacheCoordinator{
+	
+	CacheFactory cacheFactory;
+	
+	public LocalCacheCoordinator() {}
+
+	public LocalCacheCoordinator(CacheFactory cacheFactory) {
+		this.cacheFactory = cacheFactory;
+	}
 
 	/** The cache map thread local. */
-	@SuppressWarnings("rawtypes")
 	ThreadLocal<Map<Integer,Cache>> cacheMapThreadLocal = new ThreadLocal<Map<Integer,Cache>>(); 
 	
 	/* (non-Javadoc)
 	 * @see com.cetsoft.imcache.cache.coordinator.CacheCoordinator#getCache(com.cetsoft.imcache.cache.CacheType)
 	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@SuppressWarnings("unchecked")
 	public <K, V> Cache<K, V> getCache(CacheType<K, V> type) {
-		Map<Integer,Cache> cacheMap = cacheMapThreadLocal.get();
-		if(cacheMap==null){
-			cacheMapThreadLocal.set(new HashMap<Integer, Cache>());
+		Map<Integer,Cache> cacheMap = getOrCreate();
+		Cache<K,V> cache = cacheMap.get(type.getType());
+		if(cacheFactory!=null&&cache==null){
+			cache = cacheFactory.create();
+			addCache(type, cache);
 		}
-		return cacheMap.get(type.getType());
+		return cache;
 	}
 
 	/* (non-Javadoc)
 	 * @see com.cetsoft.imcache.cache.coordinator.CacheCoordinator#addCache(com.cetsoft.imcache.cache.CacheType, com.cetsoft.imcache.cache.Cache)
 	 */
-	@SuppressWarnings("rawtypes")
 	public <K, V> void addCache(CacheType<K, V> type, Cache<K, V> cache) {
-		Map<Integer,Cache> cacheMap = cacheMapThreadLocal.get();
-		if(cacheMap==null){
-			cacheMapThreadLocal.set(new HashMap<Integer, Cache>());
-		}
+		Map<Integer,Cache> cacheMap = getOrCreate();
 		cacheMap.put(type.getType(), cache);
 	}
 
+	protected Map<Integer, Cache> getOrCreate() {
+		Map<Integer, Cache> cacheMap = cacheMapThreadLocal.get();
+		if(cacheMap==null){
+			cacheMap = new HashMap<Integer, Cache>();
+			cacheMapThreadLocal.set(new HashMap<Integer, Cache>());
+		}
+		return cacheMap;
+	}
 }
