561e40e98c749694dfd19a7d36df34c7315195fc
Yusuf Aytas <yusufaytas@gmail.com>
2013-12-31 13:45:47 +0200

    bug fix for null cache return on getCache method.

--- imcache-examples/src/main/java/com/cetsoft/imcache/examples/SpringCacheExample.java
+++ imcache-examples/src/main/java/com/cetsoft/imcache/examples/SpringCacheExample.java
@@ -1,11 +1,46 @@
 package com.cetsoft.imcache.examples;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SpringCacheExample {
 
+	@Autowired
+	CacheManager cacheManager;
+	
 	public static void main(String [] args){
+		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("exampleContext.xml");
+		SpringCacheExample example = context.getBean(SpringCacheExample.class);
+		example.getBook(0);
+		example.getBook(0);
 	}
+	
+	@Cacheable("books")
+	public Book getBook(int id){
+		return new Book();
+	}
+	
+	public class Book{
+		private int id;
+		private String name;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
 }
