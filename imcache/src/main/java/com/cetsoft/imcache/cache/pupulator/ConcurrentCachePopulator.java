c5afd7f4d7a39042dd68d6a6d960072283f9960e
Yusuf Aytas <yusufaytas@gmail.com>
2014-01-04 17:21:28 +0200

    JavaDoc for populator package is added.

--- imcache/src/main/java/com/cetsoft/imcache/cache/pupulator/ConcurrentCachePopulator.java
+++ imcache/src/main/java/com/cetsoft/imcache/cache/pupulator/ConcurrentCachePopulator.java
@@ -1,3 +1,23 @@
+/*
+* Copyright (C) 2013 Yusuf Aytas, http://www.yusufaytas.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : Jan 4, 2014
+*/
 package com.cetsoft.imcache.cache.pupulator;
 
 import java.util.List;
@@ -5,20 +25,43 @@ import java.util.List;
 import com.cetsoft.imcache.cache.Cache;
 import com.cetsoft.imcache.cache.CacheEntry;
 
+/**
+ * The Class ConcurrentCachePopulator.
+ *
+ * @param <K> the key type
+ * @param <V> the value type
+ */
 public abstract class ConcurrentCachePopulator<K, V> extends AbstractCachePopulator<K, V>{
 
+	/** The concurrency level. */
 	private int concurrencyLevel;
+	
+	/** The Constant DEFAULT_CONCURRENCY_LEVEL. */
 	private final static int DEFAULT_CONCURRENCY_LEVEL = 11;
 	
+	/**
+	 * Instantiates a new concurrent cache populator.
+	 *
+	 * @param cache the cache
+	 * @param concurrencyLevel the concurrency level
+	 */
 	public ConcurrentCachePopulator(Cache<K, V> cache, int concurrencyLevel) {
 		super(cache);
 		this.concurrencyLevel = concurrencyLevel;
 	}
 	
+	/**
+	 * Instantiates a new concurrent cache populator.
+	 *
+	 * @param cache the cache
+	 */
 	public ConcurrentCachePopulator(Cache<K, V> cache) {
 		this(cache, DEFAULT_CONCURRENCY_LEVEL);
 	}
 
+	/* (non-Javadoc)
+	 * @see com.cetsoft.imcache.cache.CachePopulator#pupulate()
+	 */
 	public void pupulate() {
 		final List<CacheEntry<K, V>> entries = loadEntries();
 		for (int i = 0; i < concurrencyLevel; i++) {
--- imcache/src/main/java/com/cetsoft/imcache/cache/pupulator/LazyCachePupulator.java
+++ imcache/src/main/java/com/cetsoft/imcache/cache/pupulator/LazyCachePupulator.java
@@ -1,3 +1,23 @@
+/*
+* Copyright (C) 2013 Yusuf Aytas, http://www.yusufaytas.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : Jan 4, 2014
+*/
 package com.cetsoft.imcache.cache.pupulator;
 
 import java.util.List;
@@ -5,12 +25,26 @@ import java.util.List;
 import com.cetsoft.imcache.cache.Cache;
 import com.cetsoft.imcache.cache.CacheEntry;
 
+/**
+ * The Class LazyCachePupulator.
+ *
+ * @param <K> the key type
+ * @param <V> the value type
+ */
 public abstract class LazyCachePupulator<K,V> extends AbstractCachePopulator<K, V>{
 	
+	/**
+	 * Instantiates a new lazy cache pupulator.
+	 *
+	 * @param cache the cache
+	 */
 	public LazyCachePupulator(Cache<K, V> cache) {
 		super(cache);
 	}
 
+	/* (non-Javadoc)
+	 * @see com.cetsoft.imcache.cache.CachePopulator#pupulate()
+	 */
 	public void pupulate() {
 		new Thread(new Runnable() {
 			public void run() {
