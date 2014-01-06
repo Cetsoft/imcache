4019e4965279603f1e65f572f08f22e8b19c27c0
Yusuf Aytas <yusufaytas@gmail.com>
2014-01-06 10:21:58 +0200

    Builders are seperated from CacheBuilder.

---
+++ imcache/src/main/java/com/cetsoft/imcache/cache/builder/OffHeapCacheBuilder.java
@@ -0,0 +1,208 @@
+/*
+* Copyright (C) 2013 Yusuf Aytas, http://www.yusufaytas.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : Jan 6, 2014
+*/
+package com.cetsoft.imcache.cache.builder;
+
+import com.cetsoft.imcache.bytebuffer.OffHeapByteBufferStore;
+import com.cetsoft.imcache.cache.CacheLoader;
+import com.cetsoft.imcache.cache.EvictionListener;
+import com.cetsoft.imcache.cache.SearchableCache;
+import com.cetsoft.imcache.cache.offheap.OffHeapCache;
+import com.cetsoft.imcache.cache.search.IndexHandler;
+import com.cetsoft.imcache.cache.search.index.IndexType;
+import com.cetsoft.imcache.serialization.Serializer;
+
+/**
+ * The Class OffHeapCacheBuilder.
+ */
+public class OffHeapCacheBuilder extends CacheBuilder{
+	
+	/** The concurrency level. */
+	int concurrencyLevel;
+	
+	/** The eviction period. */
+	long evictionPeriod;
+	
+	/** The buffer cleaner period. */
+	long bufferCleanerPeriod; 
+	
+	/** The serializer. */
+	Serializer<Object> serializer;
+	
+	/** The buffer cleaner threshold. */
+	float bufferCleanerThreshold; 
+	
+	/** The byte buffer store. */
+	OffHeapByteBufferStore byteBufferStore;
+	
+	/**
+	 * Instantiates a new off heap cache builder.
+	 */
+	public OffHeapCacheBuilder(){
+		super();
+		concurrencyLevel = OffHeapCache.DEFAULT_CONCURRENCY_LEVEL;
+		evictionPeriod = OffHeapCache.DEFAULT_EVICTION_PERIOD;
+		bufferCleanerPeriod = OffHeapCache.DEFAULT_BUFFER_CLEANER_PERIOD;
+		bufferCleanerThreshold = OffHeapCache.DEFAULT_BUFFER_CLEANER_THRESHOLD;
+		serializer = SERIALIZER;
+	}
+	
+	/**
+	 * Storage.
+	 *
+	 * @param bufferStore the buffer store
+	 * @return the off heap cache builder
+	 */
+	public OffHeapCacheBuilder storage(OffHeapByteBufferStore bufferStore){
+		this.byteBufferStore = bufferStore;
+		return this;
+	}
+	
+	/**
+	 * Serializer.
+	 *
+	 * @param <V> the value type
+	 * @param serializer the serializer
+	 * @return the off heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <V> OffHeapCacheBuilder serializer(Serializer<V> serializer){
+		this.serializer = (Serializer<Object>) serializer;
+		return this;
+	}
+	
+	/**
+	 * Cache loader.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param cacheLoader the cache loader
+	 * @return the off heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> OffHeapCacheBuilder cacheLoader(CacheLoader<K, V> cacheLoader){
+		this.cacheLoader = (CacheLoader<Object, Object>) cacheLoader;
+		return this;
+	}
+	
+	/**
+	 * Eviction listener.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param evictionListener the eviction listener
+	 * @return the off heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> OffHeapCacheBuilder evictionListener(EvictionListener<K, V> evictionListener){
+		this.evictionListener = (EvictionListener<Object, Object>) evictionListener;
+		return this;
+	}
+	
+	/**
+	 * Query executer.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param indexHandler the query executer
+	 * @return the off heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> OffHeapCacheBuilder indexHandler(IndexHandler<K, V> indexHandler){
+		this.indexHandler = (IndexHandler<Object, Object>) indexHandler;
+		return this;
+	}
+	
+	/**
+	 * Concurrency level.
+	 *
+	 * @param concurrencyLevel the concurrency level
+	 * @return the off heap cache builder
+	 */
+	public OffHeapCacheBuilder concurrencyLevel(int concurrencyLevel){
+		if(concurrencyLevel>11&&concurrencyLevel<0){
+			throw new IllegalArgumentException("ConcurrencyLevel must be between 0 and 11 inclusive.");
+		}
+		this.concurrencyLevel = concurrencyLevel;
+		return this;
+	}
+	
+	/**
+	 * Eviction period.
+	 *
+	 * @param evictionPeriod the eviction period
+	 * @return the off heap cache builder
+	 */
+	public OffHeapCacheBuilder evictionPeriod(long evictionPeriod){
+		this.evictionPeriod = evictionPeriod;
+		return this;
+	}
+	
+	/**
+	 * Buffer cleaner period.
+	 *
+	 * @param bufferCleanerPeriod the buffer cleaner period
+	 * @return the off heap cache builder
+	 */
+	public OffHeapCacheBuilder bufferCleanerPeriod(long bufferCleanerPeriod){
+		this.bufferCleanerPeriod = bufferCleanerPeriod;
+		return this;
+	}
+	
+	/**
+	 * Buffer cleaner threshold.
+	 *
+	 * @param bufferCleanerThreshold the buffer cleaner threshold
+	 * @return the off heap cache builder
+	 */
+	public OffHeapCacheBuilder bufferCleanerThreshold(float bufferCleanerThreshold){
+		this.bufferCleanerThreshold = bufferCleanerThreshold;
+		return this;
+	}
+	
+	/**
+	 * Adds the index.
+	 *
+	 * @param attributeName the attribute name
+	 * @param indexType the index type
+	 * @return the off heap cache builder
+	 */
+	public OffHeapCacheBuilder addIndex(String attributeName, IndexType indexType){
+		searchable();
+		indexHandler.addIndex(attributeName, indexType);
+		return this;
+	}
+	
+	/**
+	 * Builds the cache.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @return the cache
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> SearchableCache<K, V> build() {
+		if(this.byteBufferStore==null){
+			throw new NecessaryArgumentException("ByteBufferStore must be set!");
+		}
+		return new OffHeapCache<K, V>((CacheLoader<K, V>)cacheLoader, (EvictionListener<K, V>)evictionListener,(IndexHandler<K, V>)indexHandler,
+				byteBufferStore, (Serializer<V>) serializer, bufferCleanerPeriod, bufferCleanerThreshold, concurrencyLevel, evictionPeriod);
+	}
+}
---
+++ imcache/src/main/java/com/cetsoft/imcache/cache/builder/TransactionalHeapCacheBuilder.java
@@ -0,0 +1,145 @@
+/*
+* Copyright (C) 2013 Yusuf Aytas, http://www.yusufaytas.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : Jan 6, 2014
+*/
+package com.cetsoft.imcache.cache.builder;
+
+import com.cetsoft.imcache.cache.CacheLoader;
+import com.cetsoft.imcache.cache.EvictionListener;
+import com.cetsoft.imcache.cache.SearchableCache;
+import com.cetsoft.imcache.cache.heap.TransactionalHeapCache;
+import com.cetsoft.imcache.cache.heap.tx.TransactionCommitter;
+import com.cetsoft.imcache.cache.search.IndexHandler;
+import com.cetsoft.imcache.cache.search.index.IndexType;
+
+/**
+ * The Class TransactionalHeapCacheBuilder.
+ */
+public class TransactionalHeapCacheBuilder extends CacheBuilder{
+
+	/** The capacity. */
+	private int capacity = 10000;
+	
+	/** The transaction committer. */
+	private TransactionCommitter<Object, Object> transactionCommitter;
+	
+	/**
+	 * Instantiates a new transactional heap cache builder.
+	 */
+	public TransactionalHeapCacheBuilder(){
+		super();
+	}
+	
+	/**
+	 * Transaction committer.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param transactionCommitter the transaction committer
+	 * @return the transactional heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> TransactionalHeapCacheBuilder transactionCommitter(TransactionCommitter<K, V> transactionCommitter){
+		this.transactionCommitter = (TransactionCommitter<Object, Object>) transactionCommitter;
+		return this;
+	}
+	
+	/**
+	 * Cache loader.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param cacheLoader the cache loader
+	 * @return the transactional heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> TransactionalHeapCacheBuilder cacheLoader(CacheLoader<K, V> cacheLoader){
+		this.cacheLoader = (CacheLoader<Object, Object>) cacheLoader;
+		return this;
+	}
+	
+	/**
+	 * Eviction listener.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param evictionListener the eviction listener
+	 * @return the transactional heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> TransactionalHeapCacheBuilder evictionListener(EvictionListener<K, V> evictionListener){
+		this.evictionListener = (EvictionListener<Object, Object>) evictionListener;
+		return this;
+	}
+	
+	/**
+	 * Query executer.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param indexHandler the query executer
+	 * @return the transactional heap cache builder
+	 */
+	@SuppressWarnings("unchecked")
+	public <K,V> TransactionalHeapCacheBuilder indexHandler(IndexHandler<K, V> indexHandler){
+		this.indexHandler = (IndexHandler<Object, Object>) indexHandler;
+		return this;
+	}
+	
+	/**
+	 * Capacity.
+	 *
+	 * @param capacity the capacity
+	 * @return the transactional heap cache builder
+	 */
+	public TransactionalHeapCacheBuilder capacity(int capacity){
+		this.capacity = capacity;
+		return this;
+	}
+	
+	/**
+	 * Adds the index.
+	 *
+	 * @param attributeName the attribute name
+	 * @param indexType the index type
+	 * @return the transactional heap cache builder
+	 */
+	public TransactionalHeapCacheBuilder addIndex(String attributeName, IndexType indexType){
+		searchable();
+		indexHandler.addIndex(attributeName, indexType);
+		return this;
+	}
+	
+	/**
+	 * Builds the cache.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @return the cache
+	 */
+	@SuppressWarnings("unchecked")
+	public <K, V> SearchableCache<K, V> build() {
+		if(this.transactionCommitter==null){
+			throw new NecessaryArgumentException("TransactionCommitter must be set!");
+		}
+		return new TransactionalHeapCache<K, V>((TransactionCommitter<K, V>)transactionCommitter,
+				(CacheLoader<K, V>)cacheLoader,(EvictionListener<K, V>) evictionListener,(IndexHandler<K, V>)indexHandler, capacity);
+	}
+	
+}
