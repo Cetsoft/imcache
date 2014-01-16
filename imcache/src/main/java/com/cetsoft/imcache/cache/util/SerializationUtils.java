7a992d1cb3cdebe1a07c1c25e39f11636e9b8ecc
Yusuf Aytas <yusufaytas@gmail.com>
2014-01-16 15:22:16 +0200

    Javadoc for serializationutil is added.

--- imcache/src/main/java/com/cetsoft/imcache/cache/util/SerializationUtils.java
+++ imcache/src/main/java/com/cetsoft/imcache/cache/util/SerializationUtils.java
@@ -1,7 +1,36 @@
+/*
+* Copyright (C) 2013 Yusuf Aytas, http://www.yusufaytas.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : Jan 16, 2014
+*/
 package com.cetsoft.imcache.cache.util;
 
+/**
+ * The Class SerializationUtils.
+ */
 public class SerializationUtils {
 	
+	/**
+	 * Serialize integer.
+	 *
+	 * @param integer the integer
+	 * @return the byte[]
+	 */
 	public static byte[] serializeInt(int integer){
 		byte[] ret = new byte[4];
 	    ret[3] = (byte) (integer & 0xFF);   
@@ -11,6 +40,12 @@ public class SerializationUtils {
 	    return ret;
 	}
 	
+	/**
+	 * Deserialize integer.
+	 *
+	 * @param integer the integer
+	 * @return the int
+	 */
 	public static int deserializeInt(byte[] integer){
 	    int value = 0;
 	    for (int i = 0; i < 4; i++) {
