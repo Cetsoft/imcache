3e4cea889c7aaac8e2ad061b83b1097a69508cda
yusufaytas <yusufaytas@gmail.com>
2014-05-24 23:38:18 +0100

    Versioned offheap cache is added.

--- imcache/src/main/java/com/cetsoft/imcache/cache/offheap/VersionedOffHeapCache.java
+++ imcache/src/main/java/com/cetsoft/imcache/cache/offheap/VersionedOffHeapCache.java
@@ -1,23 +1,23 @@
 /*
-* Copyright (C) 2013 Cetsoft, http://www.cetsoft.com
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Library General Public
-* License as published by the Free Software Foundation; either
-* version 2 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Library General Public License for more details.
-*
-* You should have received a copy of the GNU Library General Public
-* License along with this library; if not, write to the Free
-* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-* 
-* Author : Yusuf Aytas
-* Date   : Sep 26, 2013
-*/
+ * Copyright (C) 2013 Cetsoft, http://www.cetsoft.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * Author : Yusuf Aytas
+ * Date   : Sep 26, 2013
+ */
 package com.cetsoft.imcache.cache.offheap;
 
 import java.util.List;
@@ -37,18 +37,18 @@ import com.cetsoft.imcache.serialization.Serializer;
 /**
  * The Class VersionedOffHeapCache is a type of offheap cache where cache items
  * have versions that are incremented for each update.
- *
+ * 
  * @param <K> the key type
  * @param <V> the value type
  */
-public class VersionedOffHeapCache<K, V> implements SearchableCache<K, VersionedItem<V>>{
+public class VersionedOffHeapCache<K, V> implements SearchableCache<K, VersionedItem<V>> {
 
 	/** The name. */
 	private String name;
-	
+
 	/** The off heap cache. */
 	protected OffHeapCache<K, VersionedItem<V>> offHeapCache;
-	
+
 	/**
 	 * Instantiates a new versioned off heap cache.
 	 *
@@ -56,282 +56,335 @@ public class VersionedOffHeapCache<K, V> implements SearchableCache<K, Versioned
 	 * @param byteBufferStore the byte buffer store
 	 * @param cacheLoader the cache loader
 	 * @param evictionListener the eviction listener
-	 * @param indexHandler the query executer
+	 * @param indexHandler the index handler
 	 * @param bufferCleanerPeriod the buffer cleaner period
 	 * @param bufferCleanerThreshold the buffer cleaner threshold
 	 * @param concurrencyLevel the concurrency level
 	 * @param evictionPeriod the eviction period
 	 */
-	public VersionedOffHeapCache(Serializer<VersionedItem<V>> serializer, OffHeapByteBufferStore byteBufferStore, 
+	public VersionedOffHeapCache(Serializer<VersionedItem<V>> serializer, OffHeapByteBufferStore byteBufferStore,
 			CacheLoader<K, VersionedItem<V>> cacheLoader, EvictionListener<K, VersionedItem<V>> evictionListener,
-			IndexHandler<K, VersionedItem<V>> indexHandler,long bufferCleanerPeriod, float bufferCleanerThreshold, int concurrencyLevel, 
-			long evictionPeriod) {
-		offHeapCache = new OffHeapCache<K, VersionedItem<V>>(cacheLoader, evictionListener, indexHandler,byteBufferStore, serializer, 
+			IndexHandler<K, VersionedItem<V>> indexHandler, long bufferCleanerPeriod, float bufferCleanerThreshold,
+			int concurrencyLevel, long evictionPeriod) {
+		offHeapCache = new OffHeapCache<K, VersionedItem<V>>(cacheLoader, evictionListener, indexHandler,
+				byteBufferStore, serializer, bufferCleanerPeriod, bufferCleanerThreshold, concurrencyLevel,
+				evictionPeriod);
+	}
+
+	/**
+	 * Instantiates a new versioned off heap cache.
+	 *
+	 * @param byteBufferStore the byte buffer store
+	 * @param serializer the serializer
+	 * @param cacheLoader the cache loader
+	 * @param evictionListener the eviction listener
+	 * @param indexHandler the index handler
+	 * @param bufferCleanerPeriod the buffer cleaner period
+	 * @param bufferCleanerThreshold the buffer cleaner threshold
+	 * @param concurrencyLevel the concurrency level
+	 * @param evictionPeriod the eviction period
+	 */
+	public VersionedOffHeapCache(OffHeapByteBufferStore byteBufferStore, Serializer<V> serializer,
+			CacheLoader<K, V> cacheLoader, EvictionListener<K, V> evictionListener, IndexHandler<K, V> indexHandler,
+			long bufferCleanerPeriod, float bufferCleanerThreshold, int concurrencyLevel, long evictionPeriod) {
+		this(new CacheItemSerializer<V>(serializer), byteBufferStore, new CacheItemCacheLoader<K, V>(cacheLoader),
+				new CacheItemEvictionListener<K, V>(evictionListener), new CacheItemIndexHandler<K, V>(indexHandler),
 				bufferCleanerPeriod, bufferCleanerThreshold, concurrencyLevel, evictionPeriod);
 	}
-//	
-	/**
- * Instantiates a new versioned off heap cache.
- *
- * @param byteBufferStore the byte buffer store
- * @param serializer the serializer
- * @param cacheLoader the cache loader
- * @param evictionListener the eviction listener
- * @param indexHandler the query executer
- * @param bufferCleanerPeriod the buffer cleaner period
- * @param bufferCleanerThreshold the buffer cleaner threshold
- * @param concurrencyLevel the concurrency level
- * @param evictionPeriod the eviction period
- */
-	public VersionedOffHeapCache(OffHeapByteBufferStore byteBufferStore, Serializer<V> serializer,CacheLoader<K,V> cacheLoader, 
-			EvictionListener<K, V> evictionListener, IndexHandler<K, V> indexHandler,long bufferCleanerPeriod,float bufferCleanerThreshold, int concurrencyLevel, long evictionPeriod) {
-		this(new CacheItemSerializer<K, V>(serializer),byteBufferStore,new CacheItemCacheLoader<K, V>(cacheLoader),
-				new CacheItemEvictionListener<K, V>(evictionListener),new CacheItemIndexHandler<K,V>(indexHandler),bufferCleanerPeriod,
-				bufferCleanerThreshold,concurrencyLevel, evictionPeriod);
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.cetsoft.imcache.cache.Cache#put(java.lang.Object, java.lang.Object)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.cetsoft.imcache.cache.Cache#put(java.lang.Object,
+	 * java.lang.Object)
 	 */
 	public void put(K key, VersionedItem<V> value) {
 		int version = value.getVersion();
-		VersionedItem<V> exValue =  get(key);
-		if(exValue!=null&&version!=exValue.getVersion()){
-			throw new StaleItemException(version,exValue.getVersion());
+		VersionedItem<V> exValue = get(key);
+		if (exValue != null && version != exValue.getVersion()) {
+			throw new StaleItemException(version, exValue.getVersion());
 		}
 		version++;
 		synchronized (value) {
-			if(value.getVersion()==version){
-				throw new StaleItemException(version,exValue.getVersion());
+			if (value.getVersion() == version) {
+				throw new StaleItemException(version, exValue==null?version-1:exValue.getVersion());
 			}
 			value.setVersion(version);
+			offHeapCache.put(key, value);
 		}
-		offHeapCache.put(key, value);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.cetsoft.imcache.cache.Cache#get(java.lang.Object)
 	 */
 	public VersionedItem<V> get(K key) {
 		return offHeapCache.get(key);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.cetsoft.imcache.cache.Cache#invalidate(java.lang.Object)
 	 */
 	public VersionedItem<V> invalidate(K key) {
 		return offHeapCache.invalidate(key);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.cetsoft.imcache.cache.Cache#contains(java.lang.Object)
 	 */
 	public boolean contains(K key) {
 		return offHeapCache.contains(key);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.cetsoft.imcache.cache.Cache#clear()
 	 */
 	public void clear() {
 		offHeapCache.clear();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.cetsoft.imcache.cache.Cache#hitRatio()
 	 */
 	public double hitRatio() {
 		return offHeapCache.hitRatio();
 	}
-	
-	/* (non-Javadoc)
-	 * @see com.cetsoft.imcache.cache.SearchableCache#execute(com.cetsoft.imcache.cache.search.Query)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.cetsoft.imcache.cache.SearchableCache#execute(com.cetsoft.imcache
+	 * .cache.search.Query)
 	 */
 	public List<VersionedItem<V>> execute(Query query) {
 		return offHeapCache.execute(query);
 	}
 
 	/**
-	 * The listener interface for receiving cacheItemEviction events.
-	 * The class that is interested in processing a cacheItemEviction
-	 * event implements this interface, and the object created
-	 * with that class is registered with a component using the constructors.
-	 * When the cacheItemEviction event occurs, that object's appropriate
-	 * method is invoked.
-	 *
+	 * The listener interface for receiving cacheItemEviction events. The class
+	 * that is interested in processing a cacheItemEviction event implements
+	 * this interface, and the object created with that class is registered with
+	 * a component using the constructors. When the cacheItemEviction event
+	 * occurs, that object's appropriate method is invoked.
+	 * 
 	 * @param <K> the key type
 	 * @param <V> the value type
 	 * @see CacheItemEvictionEvent
 	 */
 	private static class CacheItemEvictionListener<K, V> implements EvictionListener<K, VersionedItem<V>> {
-		
+
 		/** The eviction listener. */
 		EvictionListener<K, V> evictionListener;
 
 		/**
 		 * Instantiates a new cache item eviction listener.
-		 *
-		 * @param evictionListener the eviction listener
+		 * 
+		 * @param evictionListener
+		 *            the eviction listener
 		 */
 		public CacheItemEvictionListener(EvictionListener<K, V> evictionListener) {
 			this.evictionListener = evictionListener;
 		}
 
-		/* (non-Javadoc)
-		 * @see com.cetsoft.imcache.cache.EvictionListener#onEviction(java.lang.Object, java.lang.Object)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.cetsoft.imcache.cache.EvictionListener#onEviction(java.lang.Object
+		 * , java.lang.Object)
 		 */
 		public void onEviction(K key, VersionedItem<V> value) {
 			evictionListener.onEviction(key, value.getValue());
 		}
 	}
-	
+
 	/**
 	 * The Class CacheItemCacheLoader.
-	 *
+	 * 
 	 * @param <K> the key type
 	 * @param <V> the value type
 	 */
-	private static class CacheItemCacheLoader<K,V> implements CacheLoader<K, VersionedItem<V>>{
+	protected static class CacheItemCacheLoader<K, V> implements CacheLoader<K, VersionedItem<V>> {
 
 		/** The cache loader. */
 		CacheLoader<K, V> cacheLoader;
-		
+
 		/**
 		 * Instantiates a new cache item cache loader.
-		 *
+		 * 
 		 * @param cacheLoader the cache loader
 		 */
 		public CacheItemCacheLoader(CacheLoader<K, V> cacheLoader) {
 			this.cacheLoader = cacheLoader;
 		}
 
-		/* (non-Javadoc)
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see com.cetsoft.imcache.cache.CacheLoader#load(java.lang.Object)
 		 */
 		public VersionedItem<V> load(K key) {
 			V value = cacheLoader.load(key);
-			if(value==null){
+			if (value == null) {
 				return null;
 			}
 			return new SimpleItem<V>(value);
 		}
-		
+
 	}
-	
+
 	/**
 	 * The Class CacheItemSerializer.
-	 *
+	 * 
 	 * @param <K> the key type
 	 * @param <V> the value type
 	 */
-	private static class CacheItemSerializer<K,V> implements Serializer<VersionedItem<V>>{
-		
+	protected static class CacheItemSerializer<V> implements Serializer<VersionedItem<V>> {
+
 		/** The serializer. */
 		Serializer<V> serializer;
 
 		/**
 		 * Instantiates a new cache item serializer.
-		 *
+		 * 
 		 * @param serializer the serializer
 		 */
 		public CacheItemSerializer(Serializer<V> serializer) {
 			this.serializer = serializer;
 		}
 
-		/* (non-Javadoc)
-		 * @see com.cetsoft.imcache.serialization.Serializer#serialize(java.lang.Object)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.cetsoft.imcache.serialization.Serializer#serialize(java.lang.
+		 * Object)
 		 */
 		public byte[] serialize(VersionedItem<V> value) {
 			byte[] payload = serializer.serialize(value.getValue());
-			byte[] newPayload = new byte[payload.length+4];
+			byte[] newPayload = new byte[payload.length + 4];
 			System.arraycopy(payload, 0, newPayload, 0, payload.length);
 			System.arraycopy(SerializationUtils.serializeInt(value.getVersion()), 0, newPayload, payload.length, 4);
 			return newPayload;
 		}
 
-		/* (non-Javadoc)
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see com.cetsoft.imcache.serialization.Serializer#deserialize(byte[])
 		 */
 		public VersionedItem<V> deserialize(byte[] payload) {
-			byte[] newPayload = new byte[payload.length-4];
+			byte[] newPayload = new byte[payload.length - 4];
 			byte[] version = new byte[4];
-			System.arraycopy(payload, 0, newPayload, 0, payload.length-4);
-			System.arraycopy(payload, payload.length-4, version, 0,4);
+			System.arraycopy(payload, 0, newPayload, 0, payload.length - 4);
+			System.arraycopy(payload, payload.length - 4, version, 0, 4);
 			SimpleItem<V> cacheItem = new SimpleItem<V>(serializer.deserialize(newPayload));
 			cacheItem.setVersion(SerializationUtils.deserializeInt(version));
 			return cacheItem;
 		}
-		
+
 	}
-	
+
 	/**
 	 * The Class CacheItemIndexHandler.
-	 *
+	 * 
 	 * @param <K> the key type
 	 * @param <V> the value type
 	 */
-	private static class CacheItemIndexHandler<K,V> implements IndexHandler<K,VersionedItem<V>>{
-		
+	protected static class CacheItemIndexHandler<K, V> implements IndexHandler<K, VersionedItem<V>> {
+
 		/** The query executer. */
 		IndexHandler<K, V> indexHandler;
 
 		/**
 		 * Instantiates a new cache item query executer.
-		 *
+		 * 
 		 * @param indexHandler the query executer
 		 */
 		public CacheItemIndexHandler(IndexHandler<K, V> indexHandler) {
 			this.indexHandler = indexHandler;
 		}
 
-		/* (non-Javadoc)
-		 * @see com.cetsoft.imcache.cache.search.Indexable#addIndex(com.cetsoft.imcache.cache.search.CacheIndex)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.cetsoft.imcache.cache.search.Indexable#addIndex(com.cetsoft.imcache
+		 * .cache.search.CacheIndex)
 		 */
 		public void addIndex(String attributeName, IndexType type) {
-			indexHandler.addIndex(attributeName,type);
+			indexHandler.addIndex(attributeName, type);
 		}
 
-		/* (non-Javadoc)
-		 * @see com.cetsoft.imcache.cache.search.IndexHandler#add(java.lang.Object, java.lang.Object)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.cetsoft.imcache.cache.search.IndexHandler#add(java.lang.Object,
+		 * java.lang.Object)
 		 */
 		public void add(K key, VersionedItem<V> value) {
 			this.indexHandler.add(key, value.getValue());
 		}
 
-		/* (non-Javadoc)
-		 * @see com.cetsoft.imcache.cache.search.IndexHandler#remove(java.lang.Object, java.lang.Object)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.cetsoft.imcache.cache.search.IndexHandler#remove(java.lang.Object
+		 * , java.lang.Object)
 		 */
 		public void remove(K key, VersionedItem<V> value) {
 			this.indexHandler.remove(key, value.getValue());
 		}
 
-		/* (non-Javadoc)
+		/*
+		 * (non-Javadoc)
+		 * 
 		 * @see com.cetsoft.imcache.cache.search.IndexHandler#clear()
 		 */
 		public void clear() {
 			this.indexHandler.clear();
 		}
 
-		/* (non-Javadoc)
-		 * @see com.cetsoft.imcache.cache.search.IndexHandler#execute(com.cetsoft.imcache.cache.search.Query)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * com.cetsoft.imcache.cache.search.IndexHandler#execute(com.cetsoft
+		 * .imcache.cache.search.Query)
 		 */
 		public List<K> execute(Query query) {
 			return indexHandler.execute(query);
 		}
-		
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.cetsoft.imcache.cache.Cache#getName()
 	 */
 	public String getName() {
-		if(this.name!=null){
+		if (this.name != null) {
 			return name;
 		}
 		return this.getClass().getName();
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.cetsoft.imcache.cache.Cache#setName(java.lang.String)
 	 */
 	public void setName(String name) {
---
+++ imcache/src/test/java/com/cetsoft/imcache/cache/offheap/VersionedOffHeapCacheTest.java
@@ -0,0 +1,170 @@
+/*
+* Copyright (C) 2013 Cetsoft, http://www.cetsoft.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : May 24, 2014
+*/
+package com.cetsoft.imcache.cache.offheap;
+
+import java.util.Random;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentMap;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import com.cetsoft.imcache.bytebuffer.OffHeapByteBuffer;
+import com.cetsoft.imcache.bytebuffer.OffHeapByteBufferStore;
+import com.cetsoft.imcache.bytebuffer.Pointer;
+import com.cetsoft.imcache.cache.CacheLoader;
+import com.cetsoft.imcache.cache.EvictionListener;
+import com.cetsoft.imcache.cache.VersionedItem;
+import com.cetsoft.imcache.cache.offheap.VersionedOffHeapCache.CacheItemSerializer;
+import com.cetsoft.imcache.cache.search.IndexHandler;
+import com.cetsoft.imcache.serialization.Serializer;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class OffHeapCacheTest.
+ */
+public class VersionedOffHeapCacheTest {
+	
+	/** The cache loader. */
+	@Mock
+	CacheLoader<Object, Object> cacheLoader;
+	
+	/** The eviction listener. */
+	@Mock
+	EvictionListener<Object, Object> evictionListener;
+	
+	/** The index handler. */
+	@Mock
+	IndexHandler<Object, Object> indexHandler;
+	
+	/** The entry. */
+	@Mock
+	Entry<Object, Pointer> entry;
+	
+	/** The serializer. */
+	@Mock
+	Serializer<Object> serializer;
+	
+	/** The buffer store. */
+	@Spy
+	OffHeapByteBufferStore bufferStore = new OffHeapByteBufferStore(1000, 10);
+	
+	/** The buffer. */
+	@Mock
+	OffHeapByteBuffer buffer;
+	
+	/** The pointer map. */
+	@Mock
+	ConcurrentMap<Object, Pointer> pointerMap;
+	
+	/** The pointer. */
+	@Mock
+	Pointer pointer;
+	
+	/** The cache. */
+	VersionedOffHeapCache<Object, Object> cache;
+	
+	/** The random. */
+	Random random = new Random();
+	
+	/** The value. */
+	@Mock
+	VersionedItem<Object> value;
+	
+	/** The ex value. */
+	@Mock
+	VersionedItem<Object> exValue;
+	
+
+	/**
+	 * Setup.
+	 */
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		cache = spy(new VersionedOffHeapCache<Object, Object>(bufferStore, serializer, cacheLoader, 
+				evictionListener, indexHandler, 100000L, 0.5f, 1, 100000L));
+		cache.offHeapCache = spy(cache.offHeapCache);
+	}
+	
+	/**
+	 * Put.
+	 */
+	@Test
+	public void put(){
+		Object object = new Object();
+		doReturn(0).when(value).getVersion();
+		doReturn(null).when(cache).get(object);
+		doNothing().when(cache.offHeapCache).put(object, value);
+		cache.put(object, value);
+		verify(cache.offHeapCache).put(object, value);
+	}
+	
+	/**
+	 * Put version are not same.
+	 */
+	@Test(expected=StaleItemException.class)
+	public void putVersionAreNotSame(){
+		Object object = new Object();
+		doReturn(0).when(value).getVersion();
+		doReturn(1).when(exValue).getVersion();
+		doReturn(exValue).when(cache).get(object);
+		cache.put(object, value);
+	}
+	
+	/**
+	 * Put version race condition.
+	 */
+	@Test(expected=StaleItemException.class)
+	public void putVersionRaceCondition(){
+		Object object = new Object();
+		doReturn(0).doReturn(1).when(value).getVersion();
+		doReturn(null).when(cache).get(object);
+		doNothing().when(cache.offHeapCache).put(object, value);
+		cache.put(object, value);
+	}
+	
+	/**
+	 * Serialize cache item serializer.
+	 */
+	@Test
+	public void serializeCacheItemSerializer(){
+		Object object = new Object();
+		CacheItemSerializer<Object> cacheItemSerializer = new CacheItemSerializer<Object>(serializer);
+		int size = 100;
+		byte[] expectedBytes = new byte[size];
+		random.nextBytes(expectedBytes);
+		doReturn(expectedBytes).when(serializer).serialize(object);
+		doReturn(object).when(serializer).deserialize(expectedBytes);
+		doReturn(object).when(value).getValue();
+		doReturn(13).when(value).getVersion();
+		byte[] bytes = cacheItemSerializer.serialize(value);
+		VersionedItem<Object> item = cacheItemSerializer.deserialize(bytes);
+		assertEquals(13, item.getVersion());
+		assertEquals(object, item.getValue());
+	}
+}
