d04835cc17307a570da1c817346a8fb835fa95d5
yusufaytas <yusufaytas@gmail.com>
2014-05-26 22:12:21 +0100

    DefaultIndexHandler is refactored and unit test is added.

--- imcache/src/test/java/com/cetsoft/imcache/cache/offheap/VersionedOffHeapCacheTest.java
+++ imcache/src/test/java/com/cetsoft/imcache/cache/offheap/VersionedOffHeapCacheTest.java
@@ -43,7 +43,6 @@ import com.cetsoft.imcache.cache.offheap.VersionedOffHeapCache.CacheItemSerializ
 import com.cetsoft.imcache.cache.search.IndexHandler;
 import com.cetsoft.imcache.serialization.Serializer;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class OffHeapCacheTest.
  */
