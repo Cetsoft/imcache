082a4d93c23c87ac632ea4aad227321ce0317c9e
yusufaytas <yusufaytas@gmail.com>
2014-06-01 23:08:01 +0100

    Several unit tests for filters are added.

--- imcache/src/main/java/com/cetsoft/imcache/cache/search/filter/LTFilter.java
+++ imcache/src/main/java/com/cetsoft/imcache/cache/search/filter/LTFilter.java
@@ -24,30 +24,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * The Class GTFilter is used to retrieve items
- * greater than the given value.
+ * The Class LEFilter is used to retrieve items
+ * less than given value.
  */
-public class GTFilter extends ArithmeticFilter {
+public class LTFilter extends ArithmeticFilter{
 
 	/**
-	 * Instantiates a new gT filter.
+	 * Instantiates a new lE filter.
 	 *
 	 * @param attributeName the attribute name
 	 * @param value the value
 	 */
-	public GTFilter(String attributeName, Object value) {
+	public LTFilter(String attributeName, Object value) {
 		super(attributeName, value);
 	}
-
+	
+	
 	/* (non-Javadoc)
-	 * @see com.cetsoft.imcache.cache.search.filter.Filter#filter(com.cetsoft.imcache.cache.search.index.CacheIndex)
+	 * @see com.cetsoft.imcache.cache.search.filter.Filter#filter(java.util.List)
 	 */
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public List<Object> filter(List<Object> objects) {
 		List<Object> result = new ArrayList<Object>(objects.size());
 		for (Object object : objects) {
 			Comparable objectValue = (Comparable)getAttributeValue(object);
-			if(objectValue.compareTo(value)>0){
+			if(objectValue.compareTo(value)<0){
 				result.add(object);
 			}
 		}
