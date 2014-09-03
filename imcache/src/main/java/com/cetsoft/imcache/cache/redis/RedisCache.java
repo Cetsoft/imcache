3d2b589273a2dc97b6d88491332dae6897e2aa4c
yusufaytas <yusufaytas@gmail.com>
2014-09-04 00:39:49 +0100

    Added licence to the redis files.

--- imcache/src/main/java/com/cetsoft/imcache/cache/redis/RedisCache.java
+++ imcache/src/main/java/com/cetsoft/imcache/cache/redis/RedisCache.java
@@ -1,3 +1,23 @@
+/*
+* Copyright (C) 2014 Cetsoft, http://www.cetsoft.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public
+* License along with this library; if not, write to the Free
+* Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+* 
+* Author : Yusuf Aytas
+* Date   : Sep 4, 2014
+*/
 package com.cetsoft.imcache.cache.redis;
 
 import com.cetsoft.imcache.cache.AbstractCache;
@@ -7,35 +27,32 @@ import com.cetsoft.imcache.cache.search.IndexHandler;
 
 public class RedisCache<K, V> extends AbstractCache<K, V> {
 
-    public RedisCache(CacheLoader<K, V> cacheLoader, EvictionListener<K, V> evictionListener, IndexHandler<K, V> indexHandler) {
-        super(cacheLoader, evictionListener, indexHandler);
-    }
+	public RedisCache(CacheLoader<K, V> cacheLoader, EvictionListener<K, V> evictionListener,
+			IndexHandler<K, V> indexHandler) {
+		super(cacheLoader, evictionListener, indexHandler);
+	}
 
-    @Override
-    public void put(K key, V value) {
-    }
+	public void put(K key, V value) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public V get(K key) {
-        return null;
-    }
+	public V get(K key) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public V invalidate(K key) {
-        return null;
-    }
+	public V invalidate(K key) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean contains(K key) {
-        return false;
-    }
+	public boolean contains(K key) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public void clear() {
-    }
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public double hitRatio() {
-        return 0;
-    }
+	public double hitRatio() {
+		throw new UnsupportedOperationException();
+	}
 }
