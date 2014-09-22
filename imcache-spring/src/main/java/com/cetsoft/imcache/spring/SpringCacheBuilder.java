9655b3e638b53d835eb20444a4bfcbf2ccd529d7
yusufaytas <yusufaytas@gmail.com>
2014-09-23 00:33:15 +0100

    Replaced the GNU licence with apache.

--- imcache-spring/src/main/java/com/cetsoft/imcache/spring/SpringCacheBuilder.java
+++ imcache-spring/src/main/java/com/cetsoft/imcache/spring/SpringCacheBuilder.java
@@ -1,23 +1,21 @@
 /*
- * Copyright (C) 2014 Yusuf Aytas, http://www.yusufaytas.com
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the Free
- * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
- * Author : Yusuf Aytas
- * Date   : Jan 6, 2014
- */
+* Copyright (C) 2014 Yusuf Aytas, http://www.yusufaytas.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Author : Yusuf Aytas
+* Date   : Jan 6, 2014
+*/
 package com.cetsoft.imcache.spring;
 
 import com.cetsoft.imcache.cache.CacheLoader;
