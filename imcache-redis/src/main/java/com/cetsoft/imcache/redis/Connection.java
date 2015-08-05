b9d618c77ab2736e1ec677b84c8a5b7acff028ec
Yusuf Aytas <yusufaytas@gmail.com>
2015-08-05 23:40:51 +0100

    Addec connection class.

---
+++ imcache-redis/src/main/java/com/cetsoft/imcache/redis/Connection.java
@@ -0,0 +1,222 @@
+/*
+* Copyright (C) 2015 Cetsoft, http://www.cetsoft.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Author : Yusuf Aytas
+* Date   : Aug 5, 2015
+*/
+package com.cetsoft.imcache.redis;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * The Class Connection.
+ */
+public class Connection {
+
+	/** The Constant DEFAULT_TIMEOUT. */
+	public final static int DEFAULT_TIMEOUT = 100;
+	
+	/** The Constant DEFAULT_SOCKET_TIMEOUT. */
+	public final static int DEFAULT_SOCKET_TIMEOUT = 100;
+	
+	/** The Constant DEFAULT_HOST. */
+	public static final String DEFAULT_HOST = "localhost";
+	
+	/** The Constant DEFAULT_PORT. */
+	public static final int DEFAULT_PORT = 6379;
+	
+	/** The host. */
+	private String host;
+	
+	/** The port. */
+	private int port;
+	
+	/** The timeout. */
+	private int timeout;
+	
+	/** The socket timeout. */
+	private int socketTimeout;
+	
+	/** The socket. */
+	Socket socket;
+	
+	/** The output stream. */
+	RedisOutputStream outputStream;
+	
+	/** The input stream. */
+	RedisInputStream inputStream;
+
+	/**
+	 * Instantiates a new connection.
+	 */
+	public Connection() {
+		this(DEFAULT_HOST, DEFAULT_PORT);
+	}
+	
+	/**
+	 * Instantiates a new connection.
+	 *
+	 * @param host the host
+	 */
+	public Connection(final String host) {
+		this(host, DEFAULT_PORT);
+	}
+
+	/**
+	 * Instantiates a new connection.
+	 *
+	 * @param host the host
+	 * @param port the port
+	 */
+	public Connection(final String host, final int port) {
+		this.host = host;
+		this.port = port;
+		this.timeout = DEFAULT_TIMEOUT;
+		this.socketTimeout = DEFAULT_SOCKET_TIMEOUT;
+	}
+
+	/**
+	 * Gets the timeout.
+	 *
+	 * @return the timeout
+	 */
+	public long getTimeout() {
+		return timeout;
+	}
+
+	/**
+	 * Sets the timeout.
+	 *
+	 * @param timeout the new timeout
+	 */
+	public void setTimeout(int timeout) {
+		this.timeout = timeout;
+	}
+
+	/**
+	 * Gets the host.
+	 *
+	 * @return the host
+	 */
+	public String getHost() {
+		return host;
+	}
+
+	/**
+	 * Gets the port.
+	 *
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * Checks if is connected.
+	 *
+	 * @return true, if is connected
+	 */
+	public boolean isConnected() {
+		return socket != null && socket.isBound() && !socket.isClosed() && socket.isConnected()
+				&& !socket.isInputShutdown() && !socket.isOutputShutdown();
+	}
+
+	/**
+	 * Opens a new connection.
+	 *
+	 * @throws ConnectionException the connection exception
+	 */
+	public void open() throws ConnectionException {
+		if (!isConnected()) {
+			try {
+				socket = createSocket();
+				socket.setReuseAddress(true);
+				// Monitor TCP connection.
+				socket.setKeepAlive(true);
+				// Make sure packets arrive timely.
+				socket.setTcpNoDelay(true);
+				socket.setSoLinger(true, 0);
+
+				socket.connect(getInetSocketAddress(), timeout);
+				socket.setSoTimeout(socketTimeout);
+				outputStream = new RedisOutputStream(socket.getOutputStream());
+				inputStream = new RedisInputStream(socket.getInputStream());
+			} catch (IOException ex) {
+				throw new ConnectionException(ex);
+			}
+		}
+	}
+
+	/**
+	 * Creates the socket.
+	 *
+	 * @return the socket
+	 */
+	protected Socket createSocket() {
+		return new Socket();
+	}
+
+	/**
+	 * Gets the inet socket address.
+	 *
+	 * @return the inet socket address
+	 */
+	protected InetSocketAddress getInetSocketAddress() {
+		return new InetSocketAddress(host, port);
+	}
+
+	/**
+	 * Close the connection.
+	 *
+	 * @throws ConnectionException the connection exception
+	 */
+	public void close() throws ConnectionException {
+		if (isConnected()) {
+			try {
+				outputStream.flush();
+				socket.close();
+			} catch (IOException ex) {
+				throw new ConnectionException(ex);
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					//ignore exception.
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Gets the output stream.
+	 *
+	 * @return the output stream
+	 */
+	public RedisOutputStream getOutputStream() {
+		return outputStream;
+	}
+
+	/**
+	 * Gets the input stream.
+	 *
+	 * @return the input stream
+	 */
+	public RedisInputStream getInputStream() {
+		return inputStream;
+	}
+
+}
---
+++ imcache-redis/src/test/java/com/cetsoft/imcache/redis/ConnectionTest.java
@@ -0,0 +1,98 @@
+/*
+* Copyright (C) 2015 Cetsoft, http://www.cetsoft.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Author : Yusuf Aytas
+* Date   : Aug 5, 2015
+*/
+package com.cetsoft.imcache.redis;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class ConnectionTest {
+	
+	Connection connection;
+	
+	@Mock
+	Socket socket;
+	
+	@Mock
+	InetSocketAddress inetSocketAddress;
+	
+	@Mock
+	RedisOutputStream redisOutputStream;
+	
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		connection = spy(new Connection());
+		connection.socket = socket;
+		connection.outputStream = redisOutputStream;
+	}
+	
+	@Test
+	public void open() throws ConnectionException, IOException{
+		doReturn(false).when(connection).isConnected();
+		doReturn(socket).when(connection).createSocket();
+		connection.open();
+		assertNotNull(connection.getInputStream());
+		assertNotNull(connection.getOutputStream());
+		verify(socket).setSoTimeout(Connection.DEFAULT_SOCKET_TIMEOUT);
+	}
+	
+	@Test(expected=ConnectionException.class)
+	public void openThrowsConnectionException() throws ConnectionException, IOException{
+		doReturn(false).when(connection).isConnected();
+		doReturn(socket).when(connection).createSocket();
+		doThrow(new SocketException()).when(socket).setSoTimeout(Connection.DEFAULT_SOCKET_TIMEOUT);
+		connection.open();
+	}
+	
+	
+	@Test(expected=ConnectionException.class)
+	public void closeThrowsConnectionException() throws ConnectionException, IOException{
+		doReturn(true).when(connection).isConnected();
+		doThrow(new IOException()).when(socket).close();
+		doNothing().when(redisOutputStream).flush();
+		connection.close();
+		verify(socket, times(2)).close();
+	}
+	
+	@Test
+	public void isConnectedReturnsFalse() throws ConnectionException, IOException{
+		doReturn(false).when(socket).isBound();
+		assertFalse(connection.isConnected());
+	}
+	
+	@Test
+	public void isConnectedReturnsTrue() throws ConnectionException, IOException{
+		doReturn(false).when(socket).isBound();
+		doReturn(false).when(socket).isClosed();
+		doReturn(true).when(socket).isConnected();
+		doReturn(false).when(socket).isInputShutdown();
+		doReturn(false).when(socket).isOutputShutdown();
+		assertFalse(connection.isConnected());
+	}
+}
