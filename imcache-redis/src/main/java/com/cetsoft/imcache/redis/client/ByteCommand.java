1cbbd8f86fe769494e1080385ba3776b70910dbd
Yusuf Aytas <yusufaytas@gmail.com>
2015-08-07 14:19:51 +0100

    Added ByteCommand interface.

---
+++ imcache-redis/src/main/java/com/cetsoft/imcache/redis/client/ByteCommand.java
@@ -0,0 +1,33 @@
+/*
+* Copyright (C) 2015 Cetsoft, http://www.cetsoft.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Author : Yusuf Aytas
+* Date   : Aug 7, 2015
+*/
+package com.cetsoft.imcache.redis.client;
+
+/**
+ * The Interface ByteCommand represent a byte command to be sent to
+ * redis server.
+ */
+public interface ByteCommand {
+	
+	/**
+	 * Gets the bytes associated with the command.
+	 *
+	 * @return the bytes
+	 */
+	byte[] getBytes();
+}
