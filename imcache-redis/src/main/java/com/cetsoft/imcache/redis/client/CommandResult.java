35f36694e702bb25e655e1973a234ff30981d22e
Yusuf Aytas <yusufaytas@gmail.com>
2015-08-11 01:22:52 +0100

    Updated interface methods for retriving command results.

--- imcache-redis/src/main/java/com/cetsoft/imcache/redis/client/CommandResult.java
+++ imcache-redis/src/main/java/com/cetsoft/imcache/redis/client/CommandResult.java
@@ -18,15 +18,38 @@
 */
 package com.cetsoft.imcache.redis.client;
 
+import java.io.IOException;
+
 /**
  * The Interface CommandResult returns the byte result of an executed command.
  */
 public interface CommandResult {
 	
 	/**
-	 * Gets the byte value of the result of an executed command.
+	 * Gets returned bytes of the result of an executed command.
 	 *
 	 * @return the byte[]
+	 * @throws ConnectionException the connection exception
+	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
-	byte[] get();
+	byte[] getBytes()  throws ConnectionException, IOException;
+	
+	/**
+	 * Gets the status of an executed command.
+	 *
+	 * @return the status
+	 * @throws ConnectionException the connection exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	String getStatus()  throws ConnectionException, IOException;
+	
+	/**
+	 * Gets the int value returned by executed command.
+	 *
+	 * @return the int
+	 * @throws ConnectionException the connection exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	int getInt()  throws ConnectionException, IOException;
+	
 }
