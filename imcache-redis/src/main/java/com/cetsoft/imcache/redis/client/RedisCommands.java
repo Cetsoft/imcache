539a44b0823ecec195b4bbebbe51e88e8536a501
Yusuf Aytas <yusufaytas@gmail.com>
2015-08-11 01:31:38 +0100

    Added initial redis commands.

---
+++ imcache-redis/src/main/java/com/cetsoft/imcache/redis/client/RedisCommands.java
@@ -0,0 +1,44 @@
+/*
+* Copyright (C) 2015 Cetsoft, http://www.cetsoft.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* 
+* Author : Yusuf Aytas
+* Date   : Aug 7, 2015
+*/
+package com.cetsoft.imcache.redis.client;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * The Enum RedisCommands are enum values of corresponding 
+ * redis commands.
+ */
+public enum RedisCommands implements ByteCommand{
+	
+	PING, SET, GET, EXPIRE, FLUSHDB, DBSIZE;
+	
+	public static final String CHARSET = "UTF-8";
+	
+	/* (non-Javadoc)
+	 * @see com.cetsoft.imcache.redis.client.ByteCommand#getBytes()
+	 */
+	public byte[] getBytes(){
+		try {
+			return this.name().getBytes(CHARSET);
+		} catch (UnsupportedEncodingException e) {
+			//do nothing.
+		}
+		return null;
+	}
+}
