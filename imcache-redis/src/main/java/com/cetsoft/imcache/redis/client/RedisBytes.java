c261fccd64dcc8dc2da6c4d1c14ef3798a5e1418
Yusuf Aytas <yusufaytas@gmail.com>
2015-08-11 01:21:37 +0100

    Added Redis bytes.

---
+++ imcache-redis/src/main/java/com/cetsoft/imcache/redis/client/RedisBytes.java
@@ -0,0 +1,14 @@
+package com.cetsoft.imcache.redis.client;
+
+public class RedisBytes {
+	
+	public static final byte CARRIAGE_RETURN_BYTE = '\r';
+	public static final byte LINE_FEED_BYTE = '\n';
+	public static final byte DASH_BYTE = '-';
+	public static final byte ASTERISK_BYTE = '*';
+	public static final byte DOLLAR_BYTE = '$';
+	public static final byte COLON_BYTE = ':';
+	public static final byte PLUS_BYTE = '+';
+	public static final byte ZERO_BYTE = '0';
+	
+}
